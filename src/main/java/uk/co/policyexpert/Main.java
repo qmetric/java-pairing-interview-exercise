3c432d498059a159a802b6963245e1961e9db57f
hatchpe <133026998+hatchpe@users.noreply.github.com>
2023-08-03 11:46:06 +0100

    Update Main.java

--- src/main/java/uk/co/policyexpert/Main.java
+++ src/main/java/uk/co/policyexpert/Main.java
@@ -8,8 +8,8 @@ public class Main {
         HomePremium homePremium = new HomePremium();
 
         CustomerProfile jCase = new CustomerProfile();
-        CustomerProfile sHomes = new CustomerProfile(); //new Customer("", LocalDate.of(1957, Month.MARCH, 4), 700), new Home(1930, 2) Name: Sherlock Homes, Date of Birth: 04/03/1957, Credit Score: 700, Home Built: 1930, Number of Bedrooms: 2
-        CustomerProfile pHolder = new CustomerProfile(); //new Customer("Polly C. Holder", LocalDate.of(1986, Month.DECEMBER, 11), 550), new Home(1930, 2)
+        CustomerProfile sHomes = new CustomerProfile();
+        CustomerProfile pHolder = new CustomerProfile();
 
         // Do not worry about changing any of the below code. Simple output is fine from the main class.
         System.out.println("====START OF OUTPUT====");
